929391d241914f6e62570993b87ad6cac808ef7d
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-15 13:02:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const age = ageInput.value.trim();
+	const name = nameInput.value.trim();
+
+	// Validation
+	if(age === "" || name === "") {
+		alert("Please enter valid details.");
+		return;
+	}
+
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(Number(age) > 18) {
+				resolve(`Welcome, ${name}. You can vote.`);
+			} else {
+				reject(`Oh sorry ${name}. You are't old enough.`);
+			}
+		}, 4000);
+	});
+	 promise
+    .then(message => alert(message))
+    .catch(message => alert(message));
+})
